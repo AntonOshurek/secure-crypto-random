30bf53b6528eb995e29a56934792f3c9611c748b
Anton Oshurek <oshurekweb@gmail.com>
2023-06-30 14:53:23 +0200

    create class for library

---
+++ secure-crypto-random.js
@@ -0,0 +1,96 @@
+import { shuffleString, createWordsArrayFromString } from './utils.js';
+
+class SecureCryptoRandom {
+	#uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	#lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+	#numberChars = "0123456789";
+	#symbolChars = "!@#$%^&*()-=_+[]{}|;:,.<>?";
+
+	cryptoRandom() {
+		if (window.crypto && window.crypto.getRandomValues) {
+			return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
+		} else {
+			console.error('[cryptoRandom] window.crypto - undefined');
+			return Math.random();
+		};
+	};
+
+	getRandomInt() {
+		return Math.floor(this.cryptoRandom() * 10);
+	};
+
+	getRandomIndex(range) {
+		if(Math.sign(range) === -1 || Math.sign(range) === 0 ) {
+			throw new Error(`Error in getRandomIndex function. range cannot be negative or zero!`);
+		};
+
+		return Math.floor(this.cryptoRandom() * range);
+	};
+
+	getRandomChar(charCase) {
+		switch(charCase) {
+			case 'upper':
+				return this.#uppercaseChars[this.getRandomIndex(this.#uppercaseChars.length)];
+			case 'lower':
+				return this.#lowercaseChars[this.getRandomIndex(this.#lowercaseChars.length)];
+			default:
+				throw new Error('unknow value in getRandomChar function. You can use only "upper" or "lower" cases in funtion parametrs!');
+		};
+	};
+
+	createPassword({passLength, uppercase, lowercase, numbers, symbols, userString}) {
+		let availableChars = "";
+		if (uppercase) availableChars += this.#uppercaseChars;
+		if (lowercase) availableChars += this.#lowercaseChars;
+		if (numbers) availableChars += this.#numberChars;
+		if (symbols) availableChars += this.#symbolChars;
+
+		const fullPassLength = userString ? passLength - userString.length : passLength;
+
+		if(Math.sign(fullPassLength) === -1 || Math.sign(fullPassLength) === 0) {
+			throw new Error(`pass length don\'t be a negative or zero. You set passLength - ${passLength} ${userString ? 'and extra line - \'' + userString + '\'.' : null} After Range = ${fullPassLength} symbols`);
+		};
+
+		const passSymbolsArray = [];
+
+		for (let i = 0; i < fullPassLength; i++) {
+			const shuffledChars = shuffleString(availableChars);
+			const randomIndex = this.getRandomIndex(shuffledChars.length);
+
+			passSymbolsArray.push(shuffledChars[randomIndex]);
+		};
+
+		if(userString && userString.length > 0) {
+			const userWords = createWordsArrayFromString(userString);
+			const userWordsCount = userWords.length;
+			const indexForUserWord = [];
+
+			for(let i = 0; i < userWordsCount; i++) {
+				indexForUserWord.push(this.getRandomIndex(fullPassLength));
+			};
+
+			for(let i = 0; i < indexForUserWord.length; i++) {
+				const indexToInsert = indexForUserWord[i];
+				const wordToInsert = userWords[i];
+
+				passSymbolsArray.splice(indexToInsert, 0, wordToInsert);
+			};
+		};
+
+		const password = passSymbolsArray.join('');
+		return password;
+	};
+};
+
+const secureCryptoRandom = new SecureCryptoRandom();
+
+// const generatePasswordParams = {
+// 	passLength: 15,
+// 	uppercase: false,
+// 	lowercase: false,
+// 	numbers: true,
+// 	symbols: true,
+// 	userString: 'Anton Ashurek'
+// };
+
+export default secureCryptoRandom;
